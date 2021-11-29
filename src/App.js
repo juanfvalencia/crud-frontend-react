1406c674d34ceaa1043f3ffc0a4de315ff4f2525
juanfvalencia <90592176+juanfvalencia@users.noreply.github.com>
2021-11-29 16:38:33 -0500

    Se agrega uso de useRef, useState y createContext

--- src/App.js
+++ src/App.js
@@ -1,14 +1,62 @@
-import React,{useContext, useReducer} from 'react';
+import React,{useContext, useReducer, useEffect, useRef, useState, createContext} from 'react';
 
 const HOST_API = "http://localhost:8080/api"
 const initialState = {
-  list: [], 
-  item: {}
-}
+  list: []
+};
 const Store = createContext(initialState)
+
+const Form = () => {
+  const formRef = useRef(null);
+  const { dispatch } = useContext(Store);
+  const [state, setState] = useState
+
+  const onAdd = (event) => {
+    event.preventDefault();
+
+    const request = {
+      name: state.name,
+      id: null,
+      isCompleted: false
+    };
+  
+    fetch(HOST_API + "/todo", {
+      method: "POST",
+      body: JSON.stringify(request),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(response => response.json())
+      .then((todo) => {
+        dispatch({ type: "add-item", item: todo });
+        setState({ name: "" });
+        formRef.current.reset();
+      });
+  }
+
+  return <form ref= {formRef}>
+    <input type= "text" name = "name" onChange={(event)=> {
+      setState({...state, name: event.target.value})
+    }}></input>
+    <input type= "text" name = "name" onChange={(event)=> {
+      setState({...state, name: event.target.value})
+    }}></input>
+    <button onClick={onAdd}>Agregar</button>
+  </form>
+
+}
 
 const List = () => {
   const{dispatch, state} = useContext(Store);
+
+  useEffect(() =>{
+    fetch(HOST_API+"/todos")
+    .then(response => response.json())
+    .then((list) => {
+      dispatch({type: "update-list", list})
+    })
+  }, [state.list.length, dispatch]);
 
   <div>
   <table >
@@ -46,9 +94,6 @@ function reducer(state, action) {
 }
 
 const StoreProvider = ({children}) => {
-
-  
-
   const [state, dispatch] = useReducer(reducer, initialState);
 
   return <Store.Provider value = {{state,dispatch}}>
@@ -59,7 +104,10 @@ const StoreProvider = ({children}) => {
 
 function App() {
   return (
-    <div></div>
+    <StoreProvider>
+      <Form/>
+      <List/>
+    </StoreProvider>
   );
 }
 
