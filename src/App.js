ddebc67ad7c9522d3b1c36a8f125aa0d21ec8549
juanfvalencia <90592176+juanfvalencia@users.noreply.github.com>
2021-11-29 16:09:12 -0500

    Se crea el template y la funcion reducer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,65 @@
-import React from 'react';
+import React,{useContext, useReducer} from 'react';
+
+const HOST_API = "http://localhost:8080/api"
+const initialState = {
+  list: [], 
+  item: {}
+}
+const Store = createContext(initialState)
+
+const List = () => {
+  const{dispatch, state} = useContext(Store);
+
+  <div>
+  <table >
+    <thead>
+      <tr>
+        <td>ID</td>
+        <td>Nombre</td>
+        <td>¿Completado?</td>
+      </tr>
+    </thead>
+    <tbody>
+      {state.list.map((todo) => {
+        return <tr key={todo.id}>
+          <td>{todo.id}</td>
+          <td>{todo.name}</td>
+          <td>{todo.isComplete}</td>
+        </tr>
+      })}
+    </tbody>
+  </table>
+</div>
+}
+
+
+
+const StoreProvider = ({children}) => {
+
+  function reducer(state, action) {
+    switch (action.type) {
+      case 'update-list':
+        return {...state, list: action.list}
+      case 'add-item':
+      const newList = state.list;
+      newList.push(action.item);
+      return {...state, list: newList}
+    default:
+      return state;
+    }
+  }
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return <Store.Provider value = {{state,dispatch}}>
+    {children}
+  </Store.Provider>
+}
 
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <div></div>
   );
 }
 
